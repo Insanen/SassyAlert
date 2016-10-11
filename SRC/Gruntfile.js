9a9283620bb4548879c20a330818a18e319375db
misterzik <nick.r@insanen.com>
2016-10-10 21:00:31 -0400

    V1.1

--- SRC/Gruntfile.js
+++ SRC/Gruntfile.js
@@ -1,100 +1,66 @@
-/*************************************
-		|\/|. _|- _   ~/.| 
-		|  ||_\|_(/_|`/_||<
---------------------------------------
->>> BROUGHT TO YOU BY INSANEN.COM  <<<
---------------------------------------
-**************************************/
-
 module.exports = function(grunt) {
 
-  /*     _ _      __   
-   *	//\/\ister//_ik   
-   * ---------------------------------
-   * Load all our Grunt Tasks & Plugins
-   * matchdep will help us to do the iteration 
-   * over the dependencies GRUNT-*.
+  /*     _ _      __
+   *	//\/\ister//_ik
+   * ----------------- *
    */
-  
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  /*   
-   *  Project Configuration starts here
-   */
-   
+  // require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project Configuration starts here
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+
+    /*
+     *  You could also have a “dist” object in  which you would output compressed CSS,
+     *  or output the CSS to a different folder.
+     *  ex) compass:dev or compass:dist.
+     *
+     *  We are using compass:dev for this configuration.
+     */
 
     compass: {
-	
-  /* 
-   *  You could also have a “dist” object in  which you would output compressed CSS, 
-   *  or output the CSS to a different folder.
-   *  ex) compass:dev or compass:dist. 
-   *  
-   *  We are using compass:dev for this configuration.
-   */
-		  
       dev: {
+
+      /*
+       *  For my project configuration im using dev to look for a file called config.rb
+       *  Remember, we could also add additional target files in here, append them as separate objects.
+       * config : 'config.rb', 'config2.rb', 'config3.rb',
+       */
+
         options: {
-	          
-   /*  
-	*  For my project configuration im using dev to look for a file called config.rb
-	*  Remember, we could also add additional target files in here, append them as separate objects.
-	* config : 'config.rb', 'config2.rb', 'config3.rb',
-	*/
-			   
           config: 'config.rb',
           force: true
         }
       }
     },
-  
-  watch: {
-	  
-	/*
-	 * Please make sure to point files: to the path of your scss files location
-	 */
-	 
-      sass: {
-        files: ['sass/**/*.scss'],
-        tasks: ['compass:dev']
-      },
-		       /*    
-		        * watch and see if our javascript files have change, or new 
-		        * packages have being installed. 
-		        */
-        js: {
-        files: ['javascripts/custom.js', 'javascripts/*.js'],
-        tasks: ['uglify']
-      },
-	  			/*   
-		  		 * watch our files for changes, and reload. 
-		  		 */
-		  		 
-      livereload: {
-        files: ['config.rb','*.php', 'stylesheets/*.css', 'img/*', 'sass/*.scss'],
-        options: {
-          livereload: true
-        }
-      },
+
+     	/*
+    	 *  Please make sure to point files: to the path of your scss files location
+    	 */
+
+       watch: {
+         sass: {
+            files: ['sass/**/*.scss'],
+            tasks: ['compass:dev']
+          },
+
+	  			/*
+		  		 *  watch our files for changes, and reload.
+           */
+
+       livereload: {
+           files: ['config.rb','*.php', 'stylesheets/*.css', 'img/*', 'sass/*.scss'],
+          options: {
+              livereload: true
+          }
+        },
     }
 
   });
-  
-/* =====================================
-         _ _      __   			   
- 		//\/\ister//_ik             
- * ------------------------------------*
- * BROUGH TO YOU BY HTTP://INSANEN.COM *	
- * ------------------------------------*
- *
- * This function takes the name of the tasks you’d like to register, 
- * along with an array (or single string). 
- * The default task to run compass and compile our SaSSy
- *
- */
-			   
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.registerTask('default', 'compass');
-  
-  
-}
+
+};
